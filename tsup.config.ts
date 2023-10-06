6875ec9c4b5c31e7dd578776f5d969edb6935c24
lucas rego <lucasalvesrego.contato@gmail.com>
2023-10-06 12:43:33 -0300

    chore: setup new entries

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig((opts) => ({
-	entryPoints: ['src/index.ts'],
+	entryPoints: ['src/index.ts', 'src/base.ts'],
 	splitting: true,
 	format: ['esm', 'cjs'],
 	dts: true,
