5a62a46ff9533ddeb4b2bf5dcf7650d2f65f3a54
devlulcas <lucasalvesrego.contato@gmail.com>
2023-10-07 09:57:12 -0300

    refactor: remove reset and awaits

--- tests/mocks/server.ts
+++ tests/mocks/server.ts
@@ -7,38 +7,31 @@ const app = new Hono();
 const fakeRemoteService = new FakeRemoteService();
 
 app.get('/', async (c) => {
-	return c.json({ users: await fakeRemoteService.getUsers() });
+	const users = fakeRemoteService.getUsers();
+	console.log(users);
+	return c.json({ users });
 });
 
 app.get('/search', async (c) => {
 	const id = Number(c.req.query('id'));
-	return c.json({ user: await fakeRemoteService.getUser(id) });
+	return c.json({ user: fakeRemoteService.getUser(id) });
 });
 
 app.post('/', async (c) => {
-	console.log(c.req);
-	const user = await c.req.json().catch((e) => {
-		console.log(e);
-		return {};
-	});
-	return c.json({ user: await fakeRemoteService.createUser(user) });
+	const user = await c.req.json();
+	return c.json({ user: fakeRemoteService.createUser(user) });
 });
 
 app.put('/', async (c) => {
 	const user = await c.req.json();
-	return c.json({ user: await fakeRemoteService.updateUser(user) });
+	return c.json({ user: fakeRemoteService.updateUser(user) });
 });
 
 app.delete('/:id', async (c) => {
 	const id = Number(c.req.param('id'));
-	await fakeRemoteService.deleteUser(id);
+	fakeRemoteService.deleteUser(id);
 	return c.json({ message: 'User deleted' });
 });
-
-app.post('/reset', async (c) => {
-	await fakeRemoteService.reset();
-	return c.json({ message: 'Database reset' });
-})
 
 serve({ fetch: app.fetch, port: PORT });
 
