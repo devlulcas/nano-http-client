53c4fa7776fd562ab0237e382abd5fbdb50a4fb6
devlulcas <lucasalvesrego.contato@gmail.com>
2023-10-07 09:57:37 -0300

    fix: body parsing in the request/response stuff

--- src/base.ts
+++ src/base.ts
@@ -50,25 +50,34 @@ class NanoHttpClientBase {
 	 * @param options options that override the default options and the url to make the request to
 	 * @returns a promise that resolves to the result of the request
 	 */
-	public async request<T>(
-		options: RequestOptions<T> = { method: 'GET', url: '/' },
-	): Promise<HttpClientResult<T>> {
-		const { actions, url, ...requestInit } = options;
+	public async request<T>(options: RequestOptions<T> = { method: 'GET', url: '/' }): Promise<HttpClientResult<T>> {
+		const { url, method, actions, body, searchParams, ...requestInit } = options;
+
+		// Merge the headers
+		const newHeaders = new Headers(requestInit.headers);
+		const headers = new Headers(this.requestInit.headers);
+		for (const [key, value] of newHeaders) {
+			headers.set(key, value);
+		}
 
 		// Get the body from the request init
-		const mergedRequestInit = this.mergeRequestInit(options);
-		const body = this.getBody(mergedRequestInit);
+		const serializedBody = this.getBody({ body, headers });
 
 		// Create a new URL object with the url and the baseUrl
 		const urlObj = new URL(url, this.baseUrl);
 
 		// Add the search parameters to the url
-		const searchParams = this.getSearchParamsFromRequestInit(requestInit);
-		urlObj.search = searchParams.toString();
+		urlObj.search = this.getSearchParams({ searchParams }).toString();
 
 		try {
 			// Make the request
-			const response = await this.fetch(urlObj, requestOptions);
+			const response = await this.fetch(urlObj, {
+				...this.requestInit,
+				...requestInit,
+				method,
+				body: serializedBody,
+				headers,
+			});
 
 			// If the request was successful, return the result
 			if (response.ok) {
@@ -99,7 +108,7 @@ class NanoHttpClientBase {
 	 * @param requestInit a request init object
 	 * @returns a URLSearchParams object that can be passed to the fetch api
 	 */
-	private getSearchParamsFromRequestInit(requestInit: CustomRequestInit): URLSearchParams {
+	private getSearchParams(requestInit: Pick<CustomRequestInit, 'searchParams'>): URLSearchParams {
 		if (requestInit.searchParams instanceof URLSearchParams) return requestInit.searchParams;
 
 		const searchParams = new URLSearchParams();
@@ -116,53 +125,43 @@ class NanoHttpClientBase {
 	 * @param requestInit a request init object
 	 * @returns a BodyInit object that can be passed to the fetch api
 	 */
-	private getBody(requestInit: Pick<CustomRequestInit, "body" | "headers">): BodyInit | undefined {
+	private getBody(requestInit: Pick<CustomRequestInit, 'body' | 'headers'>): BodyInit | undefined {
 		// Skip if body is already a valid BodyInit object
 		if (
 			requestInit.body instanceof FormData ||
 			requestInit.body instanceof URLSearchParams ||
+			requestInit.body instanceof ReadableStream ||
+			requestInit.body instanceof Blob ||
+			requestInit.body instanceof ArrayBuffer ||
 			typeof requestInit.body === 'string' ||
 			typeof requestInit.body === 'undefined'
 		) {
 			return requestInit.body;
 		}
 
-		const contentType = new Headers(requestInit.headers).get('content-type') ?? '';
+		const headers = new Headers(requestInit.headers);
+		const contentType = headers.get('content-type') ?? '';
 
 		if (contentType.includes('application/json') || contentType.includes('text/plain')) {
 			return JSON.stringify(requestInit.body);
 		}
 
 		if (contentType.includes('multipart/form-data')) {
-			return Object.entries(requestInit.body as Record<string, string>).reduce(
-				(formData, [key, value]) => {
-					formData.append(key, value);
-					return formData;
-				},
-				new FormData(),
-			);
+			return Object.entries(requestInit.body as Record<string, string>).reduce((formData, [key, value]) => {
+				formData.append(key, value);
+				return formData;
+			}, new FormData());
 		}
 
 		if (contentType.includes('application/x-www-form-urlencoded')) {
 			return new URLSearchParams(requestInit.body as Record<string, string>);
 		}
 
-		return undefined;
-	}
+		if (contentType.includes('text/html')) {
+			return String(requestInit.body);
+		}
 
-	/**
-	 * Merge the default request options with the options passed to the request method.
-	 */
-	private mergeRequestInit(options?: RequestOptions<unknown>): RequestOptions<unknown> {
-		return {
-			...this.requestInit,
-			...options,
-			method: options?.method ?? "GET",
-			headers: {
-				...this.requestInit.headers,
-				...options?.headers,
-			},
-		};
+		return undefined;
 	}
 
 	/**
@@ -175,18 +174,18 @@ class NanoHttpClientBase {
 		response: Response,
 		actions?: ResponseActions<T>['actions'],
 	): Promise<HttpClientResult<T>> {
-		const contentType = response.headers?.get('content-type') ?? '';
+		const contentType = response.headers.get('content-type') ?? 'application/json';
 
-		const data = contentType.includes('application/json')
-			? await response.json()
-			: await response.text();
+		// Parse the response
+		const data = await (contentType.includes('application/json') ? response.json() : response.text());
 
-		if (actions && actions.typeGuard && actions.typeGuard(data) === false) {
-			return Err(new HttpError(response));
+		// If the response is not of the specified type, return an error
+		if (actions?.typeGuard && !actions.typeGuard(data)) {
+			return Err(new HttpError("TypeGuard didn't pass", response));
 		}
 
 		return Ok(data, response);
 	}
 }
 
-export { NanoHttpClientBase, HttpError };
+export { HttpError, NanoHttpClientBase };
